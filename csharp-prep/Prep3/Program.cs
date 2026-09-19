09cb531645f39af3754f00ea469b86f932efe254
Logan Brizzee <brizzeesales@gmail.com>
2026-09-19 13:24:31 -0600

    Created a looping magic number program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        string playAgain;
+        
+        do
+        {
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        int guessCount = 0;
+
+        Console.Write("What do you think the magic number is? Make a guess. ");
+        string valueFromUser = Console.ReadLine();
+        int guess = int.Parse(valueFromUser);
+        guessCount++;
+            while (guess != number)
+            {
+                if (guess < number)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guess > number)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                Console.Write("Guess again: ");
+                valueFromUser = Console.ReadLine();
+                guess = int.Parse(valueFromUser);
+                guessCount++;
+            }
+            Console.WriteLine($"Thats the Number! You guessed it in {guessCount} guesses!");
+            Console.WriteLine("Do you want to play again? (Yes/No) ");
+            playAgain = Console.ReadLine();
+        } while (playAgain == "Yes");
+
+        Console.WriteLine("Thanks for playing!" );
     }
 }
