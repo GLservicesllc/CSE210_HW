2fd5d4891b4b3fa1e2ce702060f3e9a24c98cd5e
Logan Brizzee <brizzeesales@gmail.com>
2026-09-19 12:44:24 -0600

    Created a grading program

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,7 +4,73 @@ class Program
 {
     static void Main(string[] args)
     {
-        // This is prep 2
-        Console.WriteLine("Hello Prep2 World!");
+       Console.Write("Enter your Grade Percentage: ");
+       string valueFromUser = Console.ReadLine();
+       int grade = int.Parse(valueFromUser);
+       string letter;
+    
+       
+       if (grade >= 90)
+        {
+            letter = "A";
+
+            if (grade % 10 >=7)
+            {
+                letter += "+";
+            }
+
+            else if (grade % 10 <= 3)
+            {
+                letter += "-";
+            }
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+
+            if (grade % 10 >=7)
+            {
+                letter += "+";
+            }
+
+            else if (grade % 10 <= 3)
+            {
+                letter += "-";
+            }
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+             
+            if (grade % 10 >=7)
+            {
+                letter += "+";
+            }
+
+            else if (grade % 10 <= 3)
+            {
+                letter += "-";
+            }
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+
+            if (grade % 10 >=7)
+            {
+                letter += "+";
+            }
+
+            else if (grade % 10 <= 3)
+            {
+                letter += "-";
+            }
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your Grade is a {letter} in the class.");
     }
 }
